5a41bb82574fa2c6de6faaf4717b46f63c25a961
Andre <andre.talpo@gmail.com>
2020-02-13 21:34:06 -0300

    Faltando coisas

--- starter_code/script.js
+++ starter_code/script.js
@@ -24,14 +24,14 @@ class CanvasField {
     this.ctx.fillStyle = 'white';
     this.ctx.fillRect(this.width - 40 - 10, 0, 10, this.height);
 
-    this.ctx.strokeStyle = 'white';
-    this.ctx.beginPath();
-    this.ctx.setLineDash([20, 25]);
-    this.ctx.lineWidth = 5;
-    this.ctx.moveTo(this.width / 2, 0);
-    this.ctx.lineTo(this.width / 2, this.height);
-    this.ctx.stroke();
-    this.ctx.closePath();
+    // this.ctx.strokeStyle = 'white';
+    // this.ctx.beginPath();
+    // this.ctx.setLineDash([20, 25]);
+    // this.ctx.lineWidth = 5;
+    // this.ctx.moveTo(this.width / 2, 0);
+    // this.ctx.lineTo(this.width / 2, this.height);
+    // this.ctx.stroke();
+    // this.ctx.closePath();
   };
 
   clearCanvas = () => {
@@ -71,50 +71,72 @@ class Car {
 }
 
 class Obstacle {
-  constructor(width, height, x, y) {
+  constructor(width, height, x, y, ctx, color) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
     this.speed;
+    this.ctx = ctx;
+    this.color = color;
+  }
+
+  drawObstacle = () => {
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
 class Game {
-  constructor(canvas, car, obstacles) {
+  constructor(canvas, car) {
     this.canvas = canvas;
     this.car = car;
-    this.obstacles = obstacles;
+    this.obstacles = [];
+    this.lines = [];
+    this.frames = 0;
   }
 
   start = () => {
-    this.createObstacles();
     this.animationCallback();
   }
 
   animationCallback = () => {
     this.canvas.clearCanvas();
     this.canvas.drawSpeedway();
+    this.updateLine();
     this.car.drawCar();
-    console.log("ta rodando")
+    this.updateObstacles();
+
     const animation = window.requestAnimationFrame(this.animationCallback);
   }
 
-  createObstacles = () => {
-    //refazer tudo
-    var x = myGameArea.canvas.width;
-    var minHeight = 20;
-    var maxHeight = 200;
-    var height = Math.floor(
-      Math.random() * (maxHeight - minHeight + 1) + minHeight
-    );
-    var minGap = 50;
-    var maxGap = 200;
-    var gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-    myObstacles.push(new Component(10, height, "green", x, 0));
-    myObstacles.push(
-      new Component(10, x - height - gap, "green", x, height + gap)
-    );
+  updateObstacles = () => {
+    for (let i = 0; i < this.obstacles.length; i++) {
+      this.obstacles[i].y += +1;
+      this.obstacles[i].drawObstacle();
+    }
+
+    this.frames += 1;
+    if (this.frames % 120 === 0) {
+      let minWidth = 20;
+      let maxWidth = 100;
+      let x = Math.floor(Math.random() * this.canvas.width);
+      let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+      // let minGap = 50;
+      // let maxGap = 200;
+      // let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+      this.obstacles.push(new Obstacle(x, 20, width, -20, this.canvas.ctx, 'red'));
+    }
+  };
+
+  updateLine = () => {
+    for (let i = 0; i < this.lines.length; i++) {
+      this.lines[i].y += 1;
+      this.lines[i].drawObstacle();
+    }
+    if (this.frames % 30 === 0) {
+      this.lines.push(new Obstacle(5, 10, this.canvas.width/2, 0, this.canvas.ctx, 'white'));
+    }
   };
 
 }
@@ -129,7 +151,6 @@ window.onload = function () {
     canvas.createField();
     const car = new Car(canvas.width / 2 - 15, canvas.height - 60, 1, canvas.ctx);
     car.createImage();
-
 
     window.onkeydown = (e) => {
       car.move(e.keyCode);
